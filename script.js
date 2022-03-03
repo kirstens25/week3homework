45530adc845715d7d2d331a00e26fc7d45e5e994
kirstens25 <97332119+kirstens25@users.noreply.github.com>
2022-03-03 18:02:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+// Assignment Code
+const generateBtn = document.querySelector("#generate");
+const inputPasswordLength =document.getElementById("input-length");
+const spanPasswordLength = document.getElementById("span-length-display");
+
+const inputLowercase = document.getElementById("input-lowercase");
+const inputUppercase = document.getElementById("input-uppercase");
+const inputNumbers = document.getElementById("input-numbers");
+const inputSymbols = document.getElementById("input-symbols");
+
+
+
+
+setPasswordLengthSpan();
+
+function setPasswordLengthSpan(){
+// the span password length should be updated
+
+// get the value of input
+const PasswordLength = inputPasswordLength.value
+console.log(PasswordLength);
+// change the text content of span
+spanPasswordLength.textContent = PasswordLength;
+}
+
+
+//when i change the slider 
+inputPasswordLength.addEventListener('input', function(event){
+
+// the span pw length should be updated
+setPasswordLengthSpan();
+});
+
+// When I click on the generate button
+generateBtn.addEventListener('click', function(event){
+  console.log(event);
+})
+// the page will ask me my password length
+const passwordlength = Number(inputPasswordLength.value);
+console.log(PasswordLength);
+
+// the page will ask me if I want to use  (done in html)
+
+// lowercase
+const wantsLowercase = inputLowercase.checked;
+console.log(wantsLowercase)
+// uppercase
+
+// symbols
+
+// numbers 
+
+// in my password
+
+
+
+// once I have selected all the options
+
+// then the app should generate the password based on the options
+
+// then show the generated password in the textarea 
